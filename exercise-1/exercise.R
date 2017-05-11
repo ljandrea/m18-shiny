84e01ccbb742272820379743b45dddf63cef9055
Your Name <LJAndrea@D-69-91-157-141.dhcp4.washington.edu>
2017-05-11 14:16:05 -0700

    completed exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,13 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Documents/2016-2017/Sp-17/INFO-201A/m18-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source('./scripts/BuildScatter.R')
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+View(iris)
+scatter <- BuildScatter(iris, 'Sepal.Length', 'Sepal.Width', 'Petal.Length', 'Iris Scatter Plot',
+             'Sepal Length', 'Petal Length')
+scatter
--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,15 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+library(dplyr)
+library(plotly)
+library(ggplot2)
+
+BuildScatter <- function(input.data, x.var, y.var, color.var, 
+                         input.title = 'Title', x.label = 'X Title', y.label = 'Y Title') {
+  scatter <- ggplot() + 
+                geom_point(mapping = aes(x = input.data[,x.var], y = input.data[,y.var], color = input.data[,color.var])) +
+                labs(title = input.title, x= x.label, y = y.label)
+  return (scatter)
+}
