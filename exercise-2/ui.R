7a03e6f14e4eb5e1e42e2f9ed5b8c05790ffaecf
Your Name <LJAndrea@D-69-91-157-141.dhcp4.washington.edu>
2017-05-11 15:13:59 -0700

    completed exercise 2

--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -4,12 +4,14 @@
 shinyUI(fluidPage(
   
   # Add a numbericInput to choose the number of observations
-  
+  numericInput("num", label = "Number of observations", value = 100),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput('color', label = 'Select Color', choices = c('red', 'blue', 'yellow', 'purple', 'orange', 'green')),
   
   # Plot the output with the name "scatter"
-  
+  mainPanel(
+    plotOutput('scatter')
+  )
   
 ))
