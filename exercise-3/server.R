69e6b30dbcb8db1c3f14499b5b74e63e7c2c993e
Your Name <LJAndrea@D-173-250-247-102.dhcp4.washington.edu>
2017-05-16 14:11:12 -0700

    completed exercise 3

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -18,6 +18,6 @@ shinyServer(function(input, output) {
   
   # Render a plotly object that returns your map
   output$map <- renderPlotly({ 
-      return(BuildMap(joined.data, 'population'))
+      return(BuildMap(joined.data, input$input))
   })
 })
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -2,6 +2,11 @@
 shinyUI(fluidPage(
   mainPanel(
     # Add a selectInput (with a proper id) that allows you to select a variable to map
+    selectInput('input', label = 'Input Choices', 
+                choices = list('Population' = 'population', 
+                               'Electoral Votes' = 'votes',
+                               'Votes / Population' = 'ratio'), 
+                selected = 'population'),
     
     # Use plotlyOutput to show your map
     plotlyOutput('map')
